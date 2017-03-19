30e72d3b27be9e0c3d28b5d877fe613ae30ea423
claudio-walser <walsercl@gmx.ch>
2017-03-19 19:45:49 +0100

    proper replacing

--- static/mail-config.php
+++ static/mail-config.php
@@ -1,10 +1,10 @@
 <?php
 
-define('SMTP_SERVER', 'SMTP_SERVER');
-define('SMTP_PORT', SMTP_PORT);
-define('SMTP_REALNAME', 'SMTP_REALNAME');
-define('SMTP_USER', 'SMTP_USER');
-define('SMTP_PASS', 'SMTP_PASS');
+define('SMTP_SERVER', 'VALUE_SMTP_SERVER');
+define('SMTP_PORT', VALUE_SMTP_PORT);
+define('SMTP_REALNAME', 'VALUE_SMTP_REALNAME');
+define('SMTP_USER', 'VALUE_SMTP_USER');
+define('SMTP_PASS', 'VALUE_SMTP_PASS');
 
 define('RECEIPIENT', 'hewal-rechtsberatung@gmail.com');
 define('RECEIPIENT_REALNAME', 'Rechtsberatung Heinz Walser');
