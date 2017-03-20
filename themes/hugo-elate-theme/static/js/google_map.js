5ed58afa35829d0953a96fe2dba40e713a5989e2
Claudio Walser <claudio.walser@srf.ch>
2017-03-20 08:55:40 +0100

    coordinates by hand

--- themes/hugo-elate-theme/static/js/google_map.js
+++ themes/hugo-elate-theme/static/js/google_map.js
@@ -30,20 +30,13 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Amatännenstrasse 9, 8882 Unterterzen'];
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
+    var latlng = new google.maps.LatLng('47.110724', '9.245718');
+    new google.maps.Marker({
+        position: latlng,
+        map: map,
+        icon: 'images/loc.png'
+    });
 
-        });
-    }
-    
 }
 google.maps.event.addDomListener(window, 'load', init);
